1c4d75e428de977455441ff679d94a63f3c0c26f
Oliwia Strzelczyk <oliwia.strzelczyk@vip.onet.pl>
2023-12-01 13:36:32 +0100

    zadanie - angular

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Routes ,RouterModule }  from '@angular/router';
+const routes: Routes =[];
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
